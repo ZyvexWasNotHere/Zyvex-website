5c18ff5a016c7ae048475f799e5881e31df85e66
Zyvex <zyvex.og@gmail.com>
2026-07-17 18:05:28 +0330

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -25,3 +25,25 @@ const observer = new IntersectionObserver(entries => {
 skills.forEach(skill => {
 	observer.observe(skill);
 });
+
+const menuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+
+menuBtn.addEventListener("click", () => {
+
+	mobileMenu.classList.toggle("active");
+
+});
+
+
+
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+
+	link.addEventListener("click", () => {
+
+		mobileMenu.classList.remove("active");
+
+	});
+
+});
